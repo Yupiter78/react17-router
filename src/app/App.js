f17eaab314b0f96456d1c71046c768a88dab332b
js-neo <yuriy.jsx@gmail.com>
2024-08-23 22:39:43 +0300

    feat: add Switch component

--- src/app/App.js
+++ src/app/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Posts from "./components/Posts";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import Stats from "./components/Stats";
 
 const App = () => {
     return (
         <>
             <NavBar />
             <h1>App</h1>
-            <Route path="/posts" component={Posts} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/login" component={Login} />
-            <Route path="/" component={Home} />
+            <Switch>
+                <Route path="/posts" component={Posts} />
+                <Route path="/dashboard/stats" component={Stats} />
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/login" component={Login} />
+                <Route path="/" exact component={Home} />
+            </Switch>
         </>
     );
 };
