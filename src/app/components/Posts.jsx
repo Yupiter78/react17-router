b84965b326066afc9d2db1f75d337c16d4b4a56e
js-neo <yuriy.jsx@gmail.com>
2024-08-28 23:40:36 +0300

    feat: add query parameter processing to the Posts component

--- src/app/components/Posts.jsx
+++ src/app/components/Posts.jsx
@@ -2,21 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 import Post from "./Post";
 import PostList from "./PostList";
+import query from "query-string";
 
 const Posts = ({
     match: {
         params: { postId }
-    }
+    },
+    location: { search }
 }) => {
     const posts = [
         { _id: 1, label: "Post 1" },
         { _id: 2, label: "Post 2" },
         { _id: 3, label: "Post 3" }
     ];
+    const queryParse = query.parse(search);
+    const slicePosts = queryParse.count
+        ? posts.slice(0, queryParse.count)
+        : posts;
+
     return postId ? (
         <Post id={postId} posts={posts} />
     ) : (
-        <PostList {...{ posts }} />
+        <PostList posts={slicePosts} />
     );
 };
 
@@ -25,6 +32,9 @@ Posts.propTypes = {
         params: PropTypes.shape({
             postId: PropTypes.string
         })
+    }),
+    location: PropTypes.shape({
+        search: PropTypes.string
     })
 };
 
